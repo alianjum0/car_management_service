b22ff7eac2082fbb3a19b4180e03d8058fa4b30d
Muhammad Ali Anjum <muhammad.anjum@spindox.it>
2022-01-21 09:22:05 +0100

    Added swagger for documentation and api testing

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import { carsRouter } from "./cars/cars.router";
 import { errorHandler } from "./middleware/error.middleware";
 import { notFoundHandler } from "./middleware/not-found.middleware";
 import { authHandler } from "./middleware/auth.middleware";
+import swaggerUi = require('swagger-ui-express');
+import fs = require('fs');
 
 dotenv.config();
 
@@ -25,6 +27,12 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
+/* Swagger files start */
+const swaggerFile: string = (process.cwd()+"/src/api/swagger.json");
+const swaggerData: string = fs.readFileSync(swaggerFile, 'utf8');
+const swaggerDocument = JSON.parse(swaggerData);
+/* Swagger files end */
+
 /**
  *  App Configuration
  */
@@ -32,6 +40,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+// swagger docs
+app.use('/api/docs', swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument));
 
 app.use(authHandler);
 
